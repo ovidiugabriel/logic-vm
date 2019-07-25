0c5ef3b3559eb70282622f8ef5126c73bdad1adc
Ovidiu <ovidiugabriel@gmail.com>
2019-07-25 15:55:57 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <limits.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include <stdio.h>
 
@@ -13,19 +14,35 @@
 #define BYTE_MAX UCHAR_MAX
 typedef uint8_t byte;
 
+#define ALLOW_BREAKPOINT(a) do { \
+    if (break_enabled && ((a) == break_at)) { \
+            raise(SIGTRAP); \
+        } \
+    } while(0)
+
+#define POP_CODE_BYTE()  g_code[i], i++
+
 enum {
+    OP_INIT,
     OP_MOV,
     OP_NOT,
     OP_AND,
-    OP_OR
+    OP_OR,
+    OP_OR_POS
 };
 
 byte g_data_memory[BYTE_MAX+1];
 
 byte g_code[] = {
     OP_NOT, 0x00, 0x00,
-    OP_MOV, 0x02, 0x00
+    OP_MOV, 0x02, 0x00,
+    OP_INIT, 0x01, 0xef,
+    OP_INIT, 0x03, 0x01,
+    OP_OR_POS, 0x01, 0x04, 0x03, 0x03
 };
+
+bool break_enabled = false;
+int break_at = 0;
 
 void dump_memory() {
     int i;
@@ -35,51 +52,88 @@ void dump_memory() {
     printf("\n\n");
 }
 
+void breakpoint_at(int addr) {
+    break_at = addr;
+    break_enabled = true;
+}
+
 int main(int argc, char const *argv[]) {
     int i = 0;
     byte op_code;
     byte dest;
     byte src1;
     byte src2;
+    byte bitpos;
 
     /* initialize memory */
     memset(g_data_memory, 0, sizeof g_data_memory);
 
-    while (true) {
+    // breakpoint_at(0x02);
 
-        for (i = 0; i < sizeof g_code; i++) {
-            op_code = g_code[i], i++;
+    while (true) {
+        i = 0;
+        while (i < sizeof g_code) {
+            op_code = POP_CODE_BYTE();
             switch (op_code) {
+                case OP_INIT: {
+                    dest = POP_CODE_BYTE();
+
+                    g_data_memory[dest] = POP_CODE_BYTE();
+                    ALLOW_BREAKPOINT(dest);
+                }
+                break;
+
                 case OP_MOV: {
-                    dest = g_code[i]; i++;
-                    src1 = g_code[i];
+                    dest = POP_CODE_BYTE();
+                    src1 = POP_CODE_BYTE();
+
                     g_data_memory[dest] = g_data_memory[src1];
+                    ALLOW_BREAKPOINT(dest);
                 }
                 break;
 
                 case OP_NOT: {
-                    dest = g_code[i]; i++;
-                    src1 = g_code[i];
+                    dest = POP_CODE_BYTE();
+                    src1 = POP_CODE_BYTE();
+
                     g_data_memory[dest] = ~g_data_memory[src1];
+                    ALLOW_BREAKPOINT(dest);
                 }
                 break;
 
                 case OP_AND: {
-                    dest = g_code[i]; i++;
-                    src1 = g_code[i]; i++;
-                    src2 = g_code[i]; i++;
+                    dest = POP_CODE_BYTE();
+                    src1 = POP_CODE_BYTE();
+                    src2 = POP_CODE_BYTE();
+
                     g_data_memory[dest] = g_data_memory[src1] & g_data_memory[src2];
+                    ALLOW_BREAKPOINT(dest);
                 }
                 break;
 
                 case OP_OR: {
-                    dest = g_code[i]; i++;
-                    src1 = g_code[i]; i++;
-                    src2 = g_code[i]; i++;
+                    dest = POP_CODE_BYTE();
+                    src1 = POP_CODE_BYTE();
+                    src2 = POP_CODE_BYTE();
+
                     g_data_memory[dest] = g_data_memory[src1] | g_data_memory[src2];
+                    ALLOW_BREAKPOINT(dest);
+                }
+                break;
+
+                case OP_OR_POS: {
+                    dest   = POP_CODE_BYTE();
+                    bitpos = POP_CODE_BYTE();
+                    src1   = POP_CODE_BYTE();
+                    src2   = POP_CODE_BYTE();
+
+                    g_data_memory[dest] |= (g_data_memory[src1] << bitpos) | (g_data_memory[src2] << bitpos);
+                    ALLOW_BREAKPOINT(dest);
                 }
                 break;
             }
+
+
         }
 
         dump_memory();
@@ -89,3 +143,4 @@ int main(int argc, char const *argv[]) {
 
     return 0;
 }
+
